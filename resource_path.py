97776554d56b43b6125803adcf8540bcdb0fe6f5
Your Name <you@example.com>
2025-04-26 22:34:48 -0400

    updated resource path to use scripts directory rather than CWD. Used Attribute error for more precise error handling

--- resource_path.py
+++ resource_path.py
@@ -6,7 +6,8 @@ def resource_path(relative_path):
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+    except AttributeError:
+        # Use the folder where this file is located
+        base_path = os.path.dirname(os.path.abspath(__file__))
 
     return os.path.join(base_path, relative_path)
