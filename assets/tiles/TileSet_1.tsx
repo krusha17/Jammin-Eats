0feee939cb2dab3b24da6ccc94ce61c6cfbf66c5
Your Name <you@example.com>
2025-04-28 11:47:52 -0400

    Cleand file locations

--- assets/tiles/TileSet_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="TileSet_1" tilewidth="32" tileheight="32" tilecount="32" columns="8">
- <image source="../../TileSet_1.png" width="256" height="128"/>
-</tileset>
