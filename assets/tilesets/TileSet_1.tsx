21fd9eace961f698aff29375a9d995d03df939a2
Your Name <you@example.com>
2025-05-30 16:25:57 -0400

    feat: add initial level 1 tilemap and tileset with collision properties

--- assets/tilesets/TileSet_1.tsx
+++ assets/tilesets/TileSet_1.tsx
@@ -1,4 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TileSet_1" tilewidth="32" tileheight="32" tilecount="32" columns="8">
  <image source="TileSet_1.png" width="256" height="128"/>
+ <tile id="2">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
